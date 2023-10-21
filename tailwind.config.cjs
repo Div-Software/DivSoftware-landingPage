bd4f8bd96c53f2b9595fffeca4e6d36e0976db47
mikejbp <migueljosebp@gmail.com>
2023-10-21 17:57:56 -0400

    [feat]: Improve App theme
    NextUI theme with TailwindCSS configurated. Also remove default styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,5 +11,26 @@ module.exports = {
 		extend: {},
 	},
 	darkMode: 'class',
-	plugins: [nextui()],
+	plugins: [
+		nextui({
+			themes: {
+				dark: {
+					colors: {
+						background: '#11071F',
+						foreground: '#ffffff',
+						primary: {
+							50: '#1A0B2E',
+							100: '#2C1250',
+							200: '#4F228D',
+							300: '#7127BA',
+							400: '#9857D3',
+							500: '#A362FF',
+							DEFAULT: '#7127BA',
+							foreground: '#ffffff',
+						},
+					},
+				},
+			},
+		}),
+	],
 };
