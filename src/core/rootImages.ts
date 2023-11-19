56904b63a5886b2d83c40020863193ce9f239dc5
mikejbp <migueljosebp@gmail.com>
2023-11-19 13:43:16 -0400

    [perf]: Add core/root images path file
    rename images files and add services folder in assets root

---
+++ src/core/rootImages.ts
@@ -0,0 +1,45 @@
+const dirRoot = '/src/assets';
+const dirServices = '/src/assets/services/';
+const dirTechnologies = '/src/assets/technologies/';
+
+/**
+ * An object that contains all the images path from the project
+ */
+export const rootImages = {
+	services: {
+		uiux: `${dirServices}uxui.webp`,
+		webdev: `${dirServices}webdev.webp`,
+		mobile: `${dirServices}mobileapps.webp`,
+		marketing: `${dirServices}marketing.webp`,
+	},
+	technologies: {
+		Ai: `${dirTechnologies}Ai.svg`,
+		angular: `${dirTechnologies}angular.svg`,
+		astro: `${dirTechnologies}astroreal.svg`,
+		css: `${dirTechnologies}css.svg`,
+		django: `${dirTechnologies}django.svg`,
+		express: `${dirTechnologies}express.svg`,
+		figma: `${dirTechnologies}figma.svg`,
+		flask: `${dirTechnologies}flask.svg`,
+		javascript: `${dirTechnologies}javascript.svg`,
+		mongodb: `${dirTechnologies}mongo.svg`,
+		next: `${dirTechnologies}next.svg`,
+		node: `${dirTechnologies}node.svg`,
+		postgresql: `${dirTechnologies}postgres.svg`,
+		python: `${dirTechnologies}python.svg`,
+		react: `${dirTechnologies}react.svg`,
+		typescript: `${dirTechnologies}typescript.svg`,
+		vue: `${dirTechnologies}vue.svg`,
+		Xd: `${dirTechnologies}Xd.svg`,
+		techLines: {
+			firstLine: `${dirTechnologies}/lines/firstline.svg`,
+			secondLine: `${dirTechnologies}/lines/secondline.svg`,
+			thirdLine: `${dirTechnologies}/lines/thirdline.svg`,
+			fourthLine: `${dirTechnologies}/lines/fourthline.svg`,
+			fifthLine: `${dirTechnologies}/lines/fifthline.svg`,
+			sixthLine: `${dirTechnologies}/lines/sixthline.svg`,
+			seventhLine: `${dirTechnologies}/lines/seventhline.svg`,
+			eighthline: `${dirTechnologies}/lines/eighthline.svg`,
+		},
+	},
+};
