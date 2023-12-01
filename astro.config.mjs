cd9bbd1e2acd368ee31a6e06fd3bb39a13c540eb
mikejbp <migueljosebp@gmail.com>
2023-11-30 21:49:52 -0400

    [feat]: Add Astro i18next integration
    Configure i18next and create a HOC to manage i18next with JSX components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/static';
+import astroI18Next from 'astro-i18next';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [react(), tailwind()],
+	integrations: [react(), tailwind(), astroI18Next()],
 	output: 'static',
 	adapter: vercel(),
 });
