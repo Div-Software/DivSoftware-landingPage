8257206c7709473dd2f5c3a08e6c67c5535b44f1
mikejbp <migueljosebp@gmail.com>
2023-12-02 18:17:32 -0400

    Add new images and update file paths

--- src/core/rootImages.ts
+++ src/core/rootImages.ts
@@ -1,8 +1,8 @@
-const dirRoot = '/assets/';
+const dirRoot = '/src/assets/';
 const dirIcons = '/assets/icons/';
-const dirProjects = '/assets/projects/';
-const dirServices = '/assets/services/';
-const dirSocial = '/assets/social/';
+const dirProjects = '/src/assets/projects/';
+const dirServices = '/src/assets/services/';
+const dirSocial = '/src/assets/social/';
 const dirTechnologies = '/assets/technologies/';
 
 /**
